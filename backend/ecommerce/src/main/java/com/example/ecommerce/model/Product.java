c3ec0c0ae50b9dd2f2d1f176c435b84d8eb193c0
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-11-24 11:34:51 +0530

    Update Product.java

--- backend/ecommerce/src/main/java/com/example/ecommerce/model/Product.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/model/Product.java
@@ -1,1 +1,21 @@
 
+@Entity
+class Product{
+      @Id
+      @GeneratedValue(StrategyType="GenerationType.IDENTITY")
+      Integer productId;
+
+      @NotBlank
+      String productName;
+
+      @NotBlank
+      @Size(min=5 , message = "Product description must have atleast 5 characters")
+      String description;
+  
+      Integer quantity;
+  
+      Integer price;
+      Integer discount;
+      Integer specialPrice;
+  
+}
