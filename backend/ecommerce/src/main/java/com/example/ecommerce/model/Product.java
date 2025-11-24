7188b8441b9d8c28b9cbb2c1b0098e6e6495d9af
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-11-24 11:44:19 +0530

    Update Product.java

--- backend/ecommerce/src/main/java/com/example/ecommerce/model/Product.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/model/Product.java
@@ -1,5 +1,8 @@
 
 @Entity
+@Data // Shortcut for @Getter/@Setter/@ToString 
+@NoArgsConstructor // No Argument constructor
+@AllArgsConstructor // Generates constructors with all the fields as the parameter 
 class Product{
       @Id
       @GeneratedValue(StrategyType="GenerationType.IDENTITY")
@@ -8,14 +11,20 @@ class Product{
       @NotBlank
       String productName;
 
+      private String image;
+
       @NotBlank
       @Size(min=5 , message = "Product description must have atleast 5 characters")
       String description;
   
       Integer quantity;
-  
       Integer price;
       Integer discount;
       Integer specialPrice;
+
+      @ManyToOne // Many products belong to one Category
+      @JoinColumn(name="category_id) // A foreign key corresponding to the parent table Category will be created in the Child table Product with the name as category_id
+      private Category category;
+      
   
 }
