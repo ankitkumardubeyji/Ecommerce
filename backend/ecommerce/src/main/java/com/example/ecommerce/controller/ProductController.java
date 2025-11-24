5950440de67cf7e0c1706e517ae8ef3d42ee6060
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-11-24 12:02:55 +0530

    Update ProductController.java

--- backend/ecommerce/src/main/java/com/example/ecommerce/controller/ProductController.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -1,5 +1,10 @@
 @RestController
+@RequestMapping("/api")
 class ProductController{
 
+    @PostMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@Valid @RequestBody ProductDTO productDTO, @PathVariable Long categoryId){
+
+    } 
 
 }
