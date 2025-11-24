df9cac4018eac155916158f2f3cd41f18ad3feee
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-11-24 12:06:38 +0530

    Update ProductDTO.java

--- backend/ecommerce/src/main/java/com/example/ecommerce/payload/ProductDTO.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/payload/ProductDTO.java
@@ -1,5 +1,24 @@
-public class ProductDTO{
 
-    
 
+@Data // Shortcut for @Getter/@Setter/@ToString 
+@NoArgsConstructor // No Argument constructor
+@AllArgsConstructor // Generates constructors with all the fields as the parameter 
+class Product{
+     
+      Integer productId;
+
+      @NotBlank
+      String productName;
+
+      private String image;
+
+      @NotBlank
+      @Size(min=5 , message = "Product description must have atleast 5 characters")
+      String description;
+  
+      Integer quantity;
+      Integer price;
+      Integer discount;
+      Integer specialPrice;
+  
 }
