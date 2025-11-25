de9b925595cfe09b0891ba3cfac473212c89866d
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-11-25 14:14:20 +0530

    Update ProductService.java

--- backend/ecommerce/src/main/java/com/example/ecommerce/service/ProductService.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/service/ProductService.java
@@ -3,5 +3,7 @@ package com.example.ecommerce.service;
 interface ProductService{
 
         ProductDTO createProduct(ProductDTO productDTO, Long categoryId);
+
+        ProductResponse getAllProducts(pageNumber,pageSize,sortBy,sortOrder,categoryName,keyword);
     
 }
