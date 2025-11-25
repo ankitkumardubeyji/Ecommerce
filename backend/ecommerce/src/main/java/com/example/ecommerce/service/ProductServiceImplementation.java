7205eaf5f431fa42588bbc472119509b1ebb17c1
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-11-25 11:29:29 +0530

    Update ProductServiceImplementation.java

--- backend/ecommerce/src/main/java/com/example/ecommerce/service/ProductServiceImplementation.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/service/ProductServiceImplementation.java
@@ -1,5 +1,22 @@
 package com.example.ecommerce.service;
 
+@Service
 class ProductServiceImplementation implements ProductService{
+
+      @Override
+      ProductDTO createProduct(ProductDTO productDTO, Long categoryId){
+          Category catgeory = categoryRespository.findById(categoryId).orElseThrow(() => new ResourceNotFoundException("category","categoryId",categoryId));
+
+          Product mappedProduct = modelMapper.map(productDTO, Product.class);
+          mappedProduct.setSpecialPrice(productDTO.getPrice()-(0.01* productDTO.getPrice() *  productDTO.getDiscount()));
+          mappedProduct.setCategoryId(categoryId);
+
+          Product savedProduct = productRepository.save(mappedProduct);
+
+          ProductDTO savedProductDTO = modelMapper.map(savedProduct, ProductDTO.class);
+
+          return savedProductDTO;
   
+      }
+    
 } 
