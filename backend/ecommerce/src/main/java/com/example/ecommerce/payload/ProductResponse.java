958ecb5a8b07a53d030b3e878f5e30beed4ede20
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-11-25 15:00:05 +0530

    Update ProductResponse.java

--- backend/ecommerce/src/main/java/com/example/ecommerce/payload/ProductResponse.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/payload/ProductResponse.java
@@ -1,1 +1,14 @@
+package com.example.ecommerce.payload;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse{
+
+      List<ProductDTO> content;
+      Integer pageNumber;
+      Integer pageSize;
+      Integer totalElements;
+      Integer totalPages;
+      boolean isLast;
+}
