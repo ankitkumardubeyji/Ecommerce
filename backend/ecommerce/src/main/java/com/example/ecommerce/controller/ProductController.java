bd7801e880793960f256487503093529831f2424
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-11-25 11:39:06 +0530

    Update ProductController.java

--- backend/ecommerce/src/main/java/com/example/ecommerce/controller/ProductController.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -16,6 +16,28 @@ class ProductController{
     	ProductDTO savedProductDTO = productService.createProduct(productDTO,categoryId);
 		return new ResponseEntity<>(savedProductDTO,HttpStatus.CREATED);
     }
-	
+
+	public ResponseEntity<CategoryResponse> getAllCategories(
+			@RequestParam(name="pageNumber",defaultValue = AppConstants.pageNumber,required = false)Integer pageNumber,
+			@RequestParam(name="pageSize",defaultValue = AppConstants.pageSize, required = false) Integer pageSize,
+			@RequestParam(name = "sortBy",defaultValue = AppConstants.SORT_CATEGORIES_BY , required = false)String sortBy,
+			@RequestParam(name="sortOrder",defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder){
+		
+		CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber,pageSize,sortBy,sortOrder);
+		return new ResponseEntity<CategoryResponse>(categoryResponse,HttpStatus.OK);
+
+	}
+
+	public ResponseEntity<ProductResponse> getAllProducts(
+			@RequestParam(name ="pageNumber", defaultValue = AppConstants.pageNumber, required=false)Integer pageNumber,
+			@RequestParam(name="pageSize",defaultValye =  AppConstants.pageSize,required=false)Integer pageSize,
+			@RequestParam(name="sortBy", defaultValue = AppConstants.SORT_PRODUCTS_BY, required = false) String sortBy,
+			@RequestParam(name="sortOrder",defaultValue =  AppConstants.SORT_DIR , required=false) String sortOrder,
+			@RequestParam(name="categoryName",defaultValue = AppConstants.CATEGORY_NAME, required=false) String categoryName,
+			@RequestParam(name="keyword , defaultValue = AppConstants.KEYWORD, required = false) String keyword){
+		}
+
+									
+									
 
 }
