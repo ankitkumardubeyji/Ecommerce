6e69ee93f59c89b1932121f71718d28cab5428b5
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-11-25 15:44:13 +0530

    Update ProductService.java

--- backend/ecommerce/src/main/java/com/example/ecommerce/service/ProductService.java
+++ backend/ecommerce/src/main/java/com/example/ecommerce/service/ProductService.java
@@ -4,6 +4,17 @@ interface ProductService{
 
         ProductDTO createProduct(ProductDTO productDTO, Long categoryId);
 
-        ProductResponse getAllProducts(pageNumber,pageSize,sortBy,sortOrder,categoryName,keyword);
+        ProductResponse getAllProducts(Integer pageNumber,Integer pageSize,String sortBy,String sortOrder,String categoryName,String keyword);
+
+        ProductResponse searchProductByCategory(Integer categoryId);
+
+        ProductResponse searchProductByKeyword(String keyword);
+
+        ProductResponse deleteProduct(Integer productId);
+
+        ProductResponse updateProduct(ProductDTO productDTO , Integer  productId);
+
+        ProductResponse updateProductImage(Integer productId, MultipartFile image);
+        
     
 }
